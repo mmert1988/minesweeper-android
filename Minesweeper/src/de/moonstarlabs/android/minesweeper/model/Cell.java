36963cdd0f93de94316d49089a8434394e5815ba
Mehmed Mert <mehmed.mert@moonstarlabs.de>
2012-10-30 16:43:49 +0100

    2 zusätzliche Zustände in Cell speichern: - durch Flag markiert oder nicht - wurde geöffnet

--- Minesweeper/src/de/moonstarlabs/android/minesweeper/model/Cell.java
+++ Minesweeper/src/de/moonstarlabs/android/minesweeper/model/Cell.java
@@ -4,6 +4,8 @@ public class Cell {
 	
 	private boolean isMined;
 	private int minedNeighbours;
+	private boolean isMarked = false;
+	private boolean isOpen = false;
 	
 	Cell(boolean isMined, int minedNeighbours) {
 		this.isMined = isMined;
@@ -24,6 +26,26 @@ public class Cell {
 	
 	public int getMinedNeighbours() {
 		return minedNeighbours;
+	}
+	
+	public void mark() {
+		isMarked = true;
+	}
+	
+	public void unmark() {
+		isMarked = false;
+	}
+	
+	public boolean isMarked() {
+		return isMarked;
+	}
+	
+	public void open() {
+		isOpen = true;
+	}
+	
+	public boolean isOpen() {
+		return isOpen;
 	}
 
 }
