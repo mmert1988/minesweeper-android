21b184c6ac0677d5d8113e024401ffe9b0888609
kececi <tkececi@yahoo.com>
2012-11-21 16:56:09 +0100

    momentan beste konfiguration für hdpi bildschirm 6x6

--- Minesweeper/src/de/moonstarlabs/android/minesweeper/game/Game.java
+++ Minesweeper/src/de/moonstarlabs/android/minesweeper/game/Game.java
@@ -121,7 +121,7 @@ public class Game implements Parcelable {
 	}
 
 	private void initGame(DifficultyLevel level) {
-		field = RectangularField.random(5, 6, 7);
+		field = RectangularField.random(6, 6, 10);
 	}
 
 	private boolean isGameWon() {
